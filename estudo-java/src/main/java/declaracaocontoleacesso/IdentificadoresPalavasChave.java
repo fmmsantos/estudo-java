b3a0e57707abc40de1663467cdb87bcd5e875d79
Gleyson Sampaio <gleyson.s@hotmail.com>
2018-12-15 17:51:41 -0200

    criando a classe IdentificadoresPalavasChave

---
+++ estudo-java/src/main/java/declaracaocontoleacesso/IdentificadoresPalavasChave.java
@@ -0,0 +1,11 @@
+package declaracaocontoleacesso;
+
+public class IdentificadoresPalavasChave {
+	public static void main(String[] args) {
+		//char = tipo primitivo que representa um caracteres;
+		//final = usado para impedir alterar o valor da variavel
+		
+		//static e final ...
+		//final versus abstract: 
+	}
+}
